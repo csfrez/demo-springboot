849b5eb1afb70eda1662d00607b2f78799e4855d
csfrez <csfrez@163.com>
2018-10-22 18:26:51 +0800

    security抽取

--- demo-springboot-client/src/main/java/com/csfrez/demospringboot/rest/DemoConsumerController.java
+++ demo-springboot-client/src/main/java/com/csfrez/demospringboot/rest/DemoConsumerController.java
@@ -1,5 +1,6 @@
 package com.csfrez.demospringboot.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,4 +19,8 @@ public class DemoConsumerController {
         return demoService.sayHello(name);
     }
     
+    @GetMapping("/hello")
+    public String hello() {       
+        return "Hello Spring Security";
+    }
 }
--- demo-springboot-security/src/main/java/com/csfrez/demospringboot/security/WebSecurityConfig.java
+++ demo-springboot-security/src/main/java/com/csfrez/demospringboot/security/WebSecurityConfig.java
@@ -1,17 +1,12 @@
 package com.csfrez.demospringboot.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -19,20 +14,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/","/home","/actuator/**","/country/**","/druid/**").permitAll()
-				.anyRequest().authenticated().and()
-			.formLogin()
-				.usernameParameter("username")
-				.passwordParameter("password")
-//				.failureForwardUrl("/login?error")
-				.loginPage("/login").permitAll().and()
-			.logout().permitAll();
+//		http.authorizeRequests()
+//				.antMatchers("/","/home","/actuator/**","/country/**","/druid/**").permitAll()
+//				.anyRequest().authenticated();
+		http.formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
+         .and()
+         .authorizeRequests().antMatchers("/","/sayHello","/actuator/**").permitAll()        // 定义哪些URL需要被保护、哪些不需要被保护
+         .anyRequest()               // 任何请求,登录后可以访问
+         .authenticated();
 	}
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/country/**","/druid/**");
+		//web.ignoring().antMatchers("/country/**","/druid/**");
 	}
 
 	@Autowired
@@ -41,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	    //inMemoryAuthentication 从内存中获取  
 	    //auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("admin").password(new BCryptPasswordEncoder().encode("123456")).roles("USER");
 	}
-	
+	/*
 	@Bean
     @Override
     public UserDetailsService userDetailsService() {
@@ -49,4 +43,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		UserDetails user = User.withDefaultPasswordEncoder().username("admin").password("654321").roles("USER").build();
         return new InMemoryUserDetailsManager(user);
     }
+	*/
 }
