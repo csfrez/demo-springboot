cbb9a874c1d93872ae259472e9884b28a294ff7d
csfrez <csfrez@163.com>
2018-10-24 10:14:51 +0800

    spring security OAuth2

--- demo-springboot-client/src/main/java/com/csfrez/demospringboot/rest/DemoConsumerController.java
+++ demo-springboot-client/src/main/java/com/csfrez/demospringboot/rest/DemoConsumerController.java
@@ -1,6 +1,9 @@
 package com.csfrez.demospringboot.rest;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,19 @@ public class DemoConsumerController {
     @GetMapping("/hello")
     public String hello() {       
         return "Hello Spring Security";
+    }
+    
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "product id : " + id;
+    }
+    
+    @GetMapping("/order/{id}")
+    public String getOrder(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "order id : " + id;
     }
 }
--- demo-springboot-security/src/main/java/com/csfrez/demospringboot/security/WebSecurityConfig.java
+++ demo-springboot-security/src/main/java/com/csfrez/demospringboot/security/WebSecurityConfig.java
@@ -1,12 +1,19 @@
 package com.csfrez.demospringboot.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -17,12 +24,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //		http.authorizeRequests()
 //				.antMatchers("/","/home","/actuator/**","/country/**","/druid/**").permitAll()
 //				.anyRequest().authenticated();
-		http.formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
-         .and()
-         .authorizeRequests().antMatchers("/","/sayHello","/actuator/**").permitAll()        // 定义哪些URL需要被保护、哪些不需要被保护
-         .anyRequest()               // 任何请求,登录后可以访问
-         .authenticated();
+//		http.formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
+//         .and()
+//         .authorizeRequests().antMatchers("/","/sayHello","/actuator/**").permitAll()        // 定义哪些URL需要被保护、哪些不需要被保护
+//         .anyRequest()               // 任何请求,登录后可以访问
+//         .authenticated();
 	}
+
+	@Bean
+   	@Override
+   	public AuthenticationManager authenticationManagerBean() throws Exception {
+		AuthenticationManager manager = super.authenticationManagerBean();
+        return manager;
+    }
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
@@ -31,17 +45,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		//auth.inMemoryAuthentication().withUser("admin").password("admin").roles("USER");
 	    //inMemoryAuthentication 从内存中获取  
 	    //auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("admin").password(new BCryptPasswordEncoder().encode("123456")).roles("USER");
 	}
-	/*
+	
+	/*@Bean
+    public static PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }*/
+	
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+	
+	
 	@Bean
     @Override
     public UserDetailsService userDetailsService() {
-        @SuppressWarnings("deprecation")
-		UserDetails user = User.withDefaultPasswordEncoder().username("admin").password("654321").roles("USER").build();
-        return new InMemoryUserDetailsManager(user);
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		String finalPassword = bCryptPasswordEncoder.encode("123456");
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user_1").password(finalPassword).authorities("USER").build());
+        manager.createUser(User.withUsername("user_2").password(finalPassword).authorities("USER").build());
+        return manager;
+		//UserDetails user = User.withDefaultPasswordEncoder().username("admin").password("123456").roles("USER").build();
+        //return new InMemoryUserDetailsManager(user, user1, user2);
     }
-	*/
+	
 }
