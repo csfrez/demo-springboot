c4728e9d9d3f7781990cf1fc117c2f626e123d67
csfrez <csfrez@163.com>
2024-09-13 18:59:48 +0800

    新增责任链示例代码

---
+++ demo-springboot-design-pattern/src/main/java/com/csfrez/demospringboot/chain/dto/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.csfrez.demospringboot.chain.dto;
+
+public class ErrorCode {
+
+    public static final String PARAM_NULL_ERROR = "301";
+
+    public static final String PARAM_SKU_NULL_ERROR = "302";
+
+    public static final String PARAM_PRICE_NULL_ERROR = "303";
+
+    public static final String PARAM_STOCK_NULL_ERROR = "304";
+
+    public static final String PARAM_PRICE_ILLEGAL_ERROR = "305";
+
+    public static final String PARAM_STOCK_ILLEGAL_ERROR = "306";
+}
